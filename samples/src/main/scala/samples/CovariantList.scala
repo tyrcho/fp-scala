a7573e2ac5dc26075f2aeaf0ce9f9c9dba31ae8a
Michel Daviot <michel@daviot.info>
2015-10-18 15:32:25 +0200

    variance examples

---
+++ samples/src/main/scala/samples/CovariantList.scala
@@ -0,0 +1,27 @@
+package samples
+
+package covariant
+
+trait List[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+
+  def prepend[U >: T](e: U): List[U] =
+    new Cons(e, this)
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object test {
+  val x: List[String] = Nil
+}
+
---
+++ samples/src/main/scala/samples/InvariantList.scala
@@ -0,0 +1,23 @@
+package samples
+
+package invariant
+
+import java.util.NoSuchElementException
+
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty = true
+  def head = throw new NoSuchElementException("Nil.head")
+  def tail = throw new NoSuchElementException("Nil.tail")
+}
+
