54ece569ec376d02da7d7fabf079b047bac2f9e8
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-04-04 17:14:02 +0200

    WIP : HEI 2017

--- exercises/src/main/scala/collections/MoviesUtil.scala
+++ exercises/src/main/scala/collections/MoviesUtil.scala
@@ -7,39 +7,40 @@ import moviesdb._
 object MoviesUtil {
   // Hint : contains
   def isInTop250(title: String): Boolean =
-    ???
+    titles.contains(title)
 
   // Hint : filter
   def findTitlesByYear(year: String): Vector[String] =
-    ???
+    titles.filter(t => t.contains(year))
   // 2. rewrite later with a for expression
 
   // Hint : filter
   def findMoviesWithRatingAtLeast(rating: Float): Vector[Movie] =
-    ???
+    movies.filter(m => m.rating >= rating)
 
   // Hint : partition
   def partitionMoviesAtRating(rating: Float): (Vector[Movie], Vector[Movie]) =
-    ???
+    movies.partition(m => m.rating <= rating)
 
   // Hint : maxBy
   def bestMovie: Movie =
-    ???
+    //    movies.sortBy(m => m.rating).last
+    movies.maxBy(m => m.rating)
 
   // Hint : sortBy, reverse, take
   def topMovies(n: Int): Vector[Movie] =
-    ???
+    movies.sortBy(m => -m.rating).take(n)
 
   def actorByName(actorName: String) = {
     // Hint : filter, head
-    ???
+    actors.find(a => a.name == actorName)
     // 2. what happens if no actor is found ? use headOption instead
     // 3. optimize with find
   }
 
   // Hint : same as previous
   def movieByTitle(title: String) = {
-    ???
+    movies.find(m => m.title == title)
   }
 
   def actorsInMovie(title: String): Vector[Actor] =
@@ -57,20 +58,20 @@ object MoviesUtil {
     //    2. rewrite with a for comprehension
   }
 
-  // Hint : ++, distinct 
+  // Hint : ++, distinct
   def moviesWithAny(actorName1: String, actorName2: String): Vector[Movie] =
     ???
 
   // Hint : map
   def titlesWith(actorName: String): Vector[String] =
     ???
-  // rewrite later with a for expression (hint : also use distinct)   
+  // rewrite later with a for expression (hint : also use distinct)
 
   //Hint : map, mkString
   def csv(movies: Vector[Movie]) = 
     ???
 
-  // Hint : ++, distinct (reuse titlesWith) 
+  // Hint : ++, distinct (reuse titlesWith)
   def titlesWithAny(actorName1: String, actorName2: String): Vector[String] =
     ???
 
@@ -79,7 +80,7 @@ object MoviesUtil {
     ???
 
   def moviesWithAny(actorNames: Vector[String]): Vector[Movie] = {
-    // Hint : filter, exists 
+    // Hint : filter, exists
     ???
     // 2. Rewrite using map, flatten and distinct instead
 
