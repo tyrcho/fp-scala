647507cea70f5176a8befd9ba86d020cbecdfdfb
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-04-19 17:09:54 +0200

    recursivity exercices

---
+++ exercises/src/main/scala/recursivity/Factorial.scala
@@ -0,0 +1,14 @@
+package recursivity
+
+import scala.annotation.tailrec
+
+object Factorial extends App {
+  println(factorial(5))
+  println(factorialTailRec(5))
+
+  def factorial(n: Int): Int =
+    ???
+
+  def factorialTailRec(i: Int, prods: Int = 1): Int =
+    ???
+}
