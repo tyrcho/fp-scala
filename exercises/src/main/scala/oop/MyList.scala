96bd8b744fb6910af5daed5abf349285339f60d6
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-04-25 10:58:29 +0200

    list

---
+++ exercises/src/main/scala/oop/MyList.scala
@@ -0,0 +1,28 @@
+package oop
+
+import java.util.NoSuchElementException
+
+trait MyList[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: MyList[T]
+}
+
+class Cons[T](val head: T, val tail: MyList[T])
+    extends MyList[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends MyList[T] {
+  def isEmpty = true
+  def head = throw new NoSuchElementException("Nil.head")
+  def tail = throw new NoSuchElementException("Nil.tail")
+}
+
+object MyDemo extends App {
+  //  val l = Nil.prepend(3)
+  //  l.prepend(2)
+  //  println(l.head)
+  //  println(l.tail.head)
+
+}
