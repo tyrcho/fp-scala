8fc13d8bd6cd16c091206b8cf39e41da2f258169
Michel Daviot <michel@colisweb.com>
2018-09-12 13:37:40 +0200

    comment factorialTailRec

--- exercises/src/main/scala/recursivity/Factorial.scala
+++ exercises/src/main/scala/recursivity/Factorial.scala
@@ -2,11 +2,11 @@ package recursivity
 
 object Factorial extends App {
   println(factorial(5))
-  println(factorialTailRec(5))
+//  println(factorialTailRec(5))
 
   def factorial(n: Int): Int =
     ???
 
-  def factorialTailRec(i: Int, prods: Int = 1): Int =
-    ???
+//  def factorialTailRec(i: Int, prods: Int = 1): Int =
+//    ???
 }
